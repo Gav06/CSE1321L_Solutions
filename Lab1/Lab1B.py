677a62094758c2b4bb0b9088237ddb194dd07de3
Gavin <72846934+Gav06@users.noreply.github.com>
2024-10-01 12:42:28 -0400

    add Lab1

---
+++ Lab1/Lab1B.py
@@ -0,0 +1,6 @@
+#==================================== Program Lab1B.py ======================================
+# Program Lab1B.py
+# Demonstrate reading a string from the user.
+# Reads a character string from the user and prints it.
+message = input ("Enter a line of text: ");
+print ("You wrote '" + message + "'");
